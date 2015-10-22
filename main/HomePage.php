193dcdfb6a731a3b03f2c297b0b5796b9ee21336
sszhu <nora.shasha.zhu@gmail.com>
2015-10-22 18:33:19 +0800

    add file

---
+++ main/HomePage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * HomePage.php
+ * 15/10/22
+ *
+ **/
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="../css/reset.css">
+	<link href="../layout.css">
+</head>
+<body>
+	
+</body>
+</html>
